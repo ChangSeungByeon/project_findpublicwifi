27510db9b28f14aa68640ba37bba079f4bb0322c
bcs <regretful2015@naver.com>
2022-12-16 17:31:43 +0900

    changes (22/12/16/ 17:31)
    - API samlple added
    - gson driver added

---
+++ src/main/java/DB/GetWifiData.java
@@ -0,0 +1,14 @@
+package DB;
+
+public class GetWifiData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ApiExplorer var = new ApiExplorer();
+//		var.main();
+
+	}
+
+}
+
